f7c966e873f448b31efafa079982a5f1c1857738
sbudisav <sbudisav@gmail.com>
2021-12-11 16:41:22 -0600

    Test commit

--- src/index.js
+++ src/index.js
@@ -6,6 +6,7 @@ import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
+    console.log("test");
     <App />
   </React.StrictMode>,
   document.getElementById('root')
